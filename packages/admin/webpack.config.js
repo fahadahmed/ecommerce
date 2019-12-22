76363860b46b13356a0668633b0143b52c48075b
Fahad Ahmed <fahad.ahmed@anz.com>
2019-12-23 10:09:00 +1100

    setting up react in admin package

---
+++ packages/admin/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const appConfig = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/,
+        use: {
+            loader: 'file-loader',
+            options: {
+                name: "./images/[hash].[ext]"
+            }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 4000,
+    open: true,
+    hot: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'The Nerdy Bits | Admin Section',
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+}
+module.exports = [appConfig];
