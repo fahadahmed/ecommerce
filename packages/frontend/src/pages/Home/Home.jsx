4ded3bd3c7985e7c1fd1955fd27c79a69a914223
Fahad Ahmed <fahad.ahmed@anz.com>
2019-12-23 12:02:38 +1100

    update to the frontend home page

--- packages/frontend/src/pages/Home/Home.jsx
+++ packages/frontend/src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
   return(
     <React.Fragment>
       <h1>The Nerdy Bits Homepage</h1>
+      <p>A place for quirky and fun developer swag!</p>
       <Link to="/products">All Products</Link>
     </React.Fragment>
   )
