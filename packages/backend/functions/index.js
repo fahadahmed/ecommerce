b783344ced466ffc120d6838901119239ecdae22
Fahad Ahmed <fahad.ahmed@anz.com>
2020-01-02 12:58:51 +1100

    created a cloud function to add user to mailchimp

--- packages/backend/functions/index.js
+++ packages/backend/functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+admin.firestore().settings({timestampInSnapshots: true});
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+const Mailchimp = require('mailchimp-api-v3');
+const mailchimp = new Mailchimp('69f2fd132d7d74d91a0043c63cfa2427-us4');
+
+exports.addUserToList = functions.firestore.document('subscribers/{pushId}').onWrite(async (change, context) => {
+  const { email, firstName } = change.after.data();
+  const res = await mailchimp.post('/lists/60554224b6/members', {
+    email_address: email,
+    status: 'subscribed',
+    merge_fields: {
+      FNAME: firstName
+    }
+  })
+  .then((results) => {
+    console.log('successully added a new firebase user', email,' ', firstName, 'to Mailchimp list', results);
+    return {status: results.statusCode, message: "User has been added successfully." };
+  })
+  .catch((err) => {
+    console.log('Mailchimp: Error while attempting to add the user', err);
+    return {status: err.status, message: "Error while attempting to add the user"};
+  })
+}) 
